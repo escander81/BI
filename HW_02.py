d92a812edac00845f9386e34711929aa0c78b907
Alexander Tarassov <tarassov.sasha@gmail.com>
2023-03-14 20:29:23 -0600

    update HW2

--- HW_02.py
+++ HW_02.py
@@ -4,8 +4,10 @@
 чтобы все монетки были повернуты вверх одной и той же стороной. 
 Выведите минимальное количество монет, которые нужно перевернуть
 """
-n = int(input("How many coins?: "))
 
+n=int(input("How many coins?: "))
+k = int(input("How many are tails: "))
+print(min(k, n-k))
 
 """
 Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница. 
@@ -15,11 +17,19 @@ n = int(input("How many coins?: "))
 Помогите Кате отгадать задуманные Петей числа.
 """
 
+s = int(input("Enter S "))
+p = int(input("Enter P "))
 
-
+for i in range (1, 1001):
+    for j in range (1, 1001):
+        if (i + j == s) and (i * j == p):
+            print(i, j)
 
 """
 Задача 14: Требуется вывести все целые степени двойки 
 (т.е. числа вида 2k), не превосходящие числа N.
 
 """
+n = int(input("What is the max limit? "))
+for i in range (1, n+1):
+    print(2**i)
